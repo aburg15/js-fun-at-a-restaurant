43c7aa52b00f6ede654aa2d6dbab5acef2a662fb
Adam Burgess <aburg1515@gmail.com>
2021-08-27 08:50:12 -0600

    Initial commit

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,47 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
+};
 
-}
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+ }
+  return menuItem;
+};
+
+function addIngredients(topping1, toppings) {
+  if (toppings.includes(topping1)) {
+    return topping1
+  } else {
+    return toppings.push(topping1)
+  }
+};
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+};
+
+function decreasePrice(decreasedPrice) {
+  return decreasedPrice * .90;
+};
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type,
+  }
+  return recipe;
+};
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
