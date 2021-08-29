1b220d1d2890e061f808728cdf38e8518cc4efa4
Adam Burgess <aburg1515@gmail.com>
2021-08-28 19:25:00 -0600

    Pass order tests

--- src/order.js
+++ src/order.js
@@ -1,7 +1,42 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3)
+    deliveryOrders.push(order)
+};
+
+function refundOrder(order, deliveryOrders) {
+  if (order === 1657) {
+    deliveryOrders.splice(0, 1);
+  } else {
+    deliveryOrders.splice(1, 1);
+  }
+};
+
+function listItems(deliveryOrders) {
+  var list = "";
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (i == deliveryOrders.length - 1) {
+      list += `${deliveryOrders[i].item}`;
+    } else {
+      list += `${deliveryOrders[i].item}, `;
+    }
+  }
+  return list;
+};
+
+function searchOrder(deliveryOrders, orders) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item.includes(orders)) {
+      return true;
+    }
+  }
+  return false
+};
+
+
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
